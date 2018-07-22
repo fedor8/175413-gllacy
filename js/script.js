44604f44951b4a325c0ab26e46966a4a4189d510
Fedor Dydykin <fedor8@mail.ru>
2018-07-23 01:05:30 +0300

    валидация формы анимация и центральный элемент высота инпутов js функции для модальных окон br fix исправления для картинки карты исправление каскадов для кнопок пользовательского меню стиль кнопок бэм фикс двойной ползунок select чекбоксы и радиокнопки при наведении фильтр Жирность фильтр: разметка + чекбоксы и кнопка

--- js/script.js
+++ js/script.js
@@ -1,26 +1,6 @@
-(function(e){
- e.closest = e.closest || function(css){
-   var node = this;
-
-   while (node) {
-      if (node.matches(css)) return node;
-      else node = node.parentElement;
-   }
-   return null;
- }
-
-  var matches = e.matches || e.matchesSelector || e.webkitMatchesSelector || e.mozMatchesSelector || e.msMatchesSelector || e.oMatchesSelector;
-  !matches ? (e.matches = e.matchesSelector = function matches(selector) {
-    var matches = document.querySelectorAll(selector);
-    var th = this;
-    return Array.prototype.some.call(matches, function(e) {
-      return e === th;
-    });
-  }) : (e.matches = e.matchesSelector = matches);
-})(Element.prototype);
-
 (function () {
-  'use strict';
+  "use strict";
+  let blckoutElement = document.querySelector(".modal-blackout");
 
   let searchButton = document.querySelector(".search-button");
   if (searchButton) {
@@ -46,22 +26,24 @@
   let feedbackButton = document.querySelector(".feedback-button");
   if (feedbackButton) {
     feedbackButton.addEventListener("click", function (event) {
-      closeActiveModalElement(event);
-      document.querySelector(".feedback-panel__wrapper").classList.remove("temporary-hidden");
-      document.querySelector(".feedback-panel").classList.remove("temporary-hidden");
-      event.preventDefault();
-      event.stopPropagation()
+      showModalElement(event, "feedback-panel", true);
     });
   }
 
   let feedbackButtonCloseButton = document.querySelector(".feedback-panel-close-button");
   if(feedbackButtonCloseButton){
     feedbackButtonCloseButton.addEventListener("click", function (event) {
-      document.querySelector(".feedback-panel__wrapper").classList.add("temporary-hidden");
-      document.querySelector(".feedback-panel").classList.add("temporary-hidden");
       event.preventDefault();
-      event.stopPropagation()
+      event.stopPropagation();
+      closeActiveModalElement();
     });
+  }
+
+  let forms = document.querySelectorAll("form");
+  if(forms){
+    for(let i = 0; i < forms.length; i++){
+      addFormValidation(forms[i]);
+    }
   }
 
 
@@ -69,20 +51,94 @@
     closeActiveModalElement(event);
   });
 
-  function closeActiveModalElement(event) {
-    let modal = document.querySelector(".modal");
-    if (modal && event.target.closest(".modal") == null) {
-      modal.classList.remove("modal");
+  function addFormValidation(form) {
+    let formInputs = form.querySelectorAll("input[type='text'],input[type='email'],input[type='search'],input[type='password']");
+    if(formInputs){
+      for(let i = 0; i < formInputs.length; i++){
+        formInputs[i].addEventListener("keydown", function (event) {
+          if (event.keyCode === 27) {
+            validateForm(form);
+          }
+        })
+      }
+    }
+    let submitButtons = form.querySelectorAll("button[type='submit']");
+    if (submitButtons) {
+      for (let i = 0; i < submitButtons.length; i++) {
+        submitButtons[i].addEventListener("click", function (event) {
+          validateForm(form);
+        })
+      }
+    }
+  }
+  function validateForm(form) {
+    if(!form.checkValidity()){
+      let panel = form.parentElement;
+      panel.classList.add("error-form");
+      setTimeout(function () {
+        panel.classList.remove("error-form");
+      }, 1000);
     }
   }
 
-  function showModalElement(event, className) {
-    closeActiveModalElement(event);
+  function closeActiveModalElement(event) {
+    let modal = document.querySelector(".modal");
+    if (!modal) {
+      return;
+    }
+    if(event){
+      if(!clickedOutside(event)){
+        return;
+      }
+    }
+    if(blckoutElement && !blckoutElement.classList.contains("temporary-hidden")){
+      blckoutElement.classList.add("temporary-hidden");
+    }
+    modal.classList.add("slide-out");
+    setTimeout(function () {
+      modal.classList.remove("modal");
+      modal.classList.remove("slide-out");
+    }, 600);
+
+  }
+
+  function clearInputs(modalElement) {
+    if(!modalElement){
+      return;
+    }
+    let inputs = modalElement.querySelectorAll("input,textarea");
+    if(inputs){
+      for (let i=0; i < inputs.length; i++) {
+        inputs[i].value = "";
+      }
+    }
+  }
+
+  function clickedOutside(event) {
+    return getParentWithClass(event.target, "modal") == null;
+  }
+
+  function getParentWithClass(node, className){
+    while (node) {
+      if (node.classList.contains(className)) return node;
+      else node = node.parentElement;
+    }
+    return null;
+  }
+
+  function showModalElement(event, className, showBlackout) {
+    event.preventDefault();
+    event.stopPropagation();
+    closeActiveModalElement();
     let element = document.querySelector("."+className);
     if (element) {
+      clearInputs(element);
       element.classList.add("modal");
+      let firstInput = element.querySelector("input,textarea");
+      firstInput.focus();
+      if(showBlackout && blckoutElement){
+        blckoutElement.classList.remove("temporary-hidden");
+      }
     }
-    event.preventDefault();
-    event.stopPropagation()
   }
 }());
